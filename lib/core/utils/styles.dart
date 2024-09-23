b947b571230cc0f6340ecf424ee5bec47beedd6d
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-09-23 06:52:28 +0300

    add styles file

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Styles {
+  static TextStyle styleBold32 = GoogleFonts.nunito(
+      fontWeight: FontWeight.bold,
+      color: const Color(0xff272727),
+      fontSize: 32);
+
+  static TextStyle styleMedium16 = GoogleFonts.nunito(
+      fontWeight: FontWeight.w500,
+      color: const Color(0xff272727),
+      fontSize: 16);
+
+  static TextStyle styleMediumWhite16 = GoogleFonts.nunito(
+      fontWeight: FontWeight.w500, color: Colors.white, fontSize: 16);
+
+  static TextStyle styleBoldNunito12 = GoogleFonts.nunito(
+      fontWeight: FontWeight.bold,
+      color: const Color(0xff272727),
+      fontSize: 12);
+
+  static TextStyle styleMedium12 = GoogleFonts.nunito(
+      fontWeight: FontWeight.w500,
+      color: const Color(0xff272727),
+      fontSize: 12);
+
+  static TextStyle styleMedium24 = GoogleFonts.nunito(
+      fontWeight: FontWeight.w500,
+      color: const Color(0xff272727),
+      fontSize: 24);
+
+  static TextStyle styleBold24 = GoogleFonts.gabarito(
+      fontWeight: FontWeight.bold,
+      color: const Color(0xff272727),
+      fontSize: 24);
+
+  static TextStyle styleBoldGarabito12 = GoogleFonts.gabarito(
+      fontWeight: FontWeight.bold,
+      color: const Color(0xff272727),
+      fontSize: 12);
+
+  static TextStyle styleBold16 = GoogleFonts.gabarito(
+      fontWeight: FontWeight.bold,
+      color: const Color(0xff272727),
+      fontSize: 16);
+}
