bcd843e1a7f2837f1dc0d8a8d12fb5e1173d1e59
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-11-04 18:19:18 +0200

    dark mode categories view done

--- lib/features/home/presentation/widgets/categories_view_list_tile.dart
+++ lib/features/home/presentation/widgets/categories_view_list_tile.dart
@@ -1,9 +1,12 @@
+import 'package:e_clot_shop/core/utils/colors.dart';
 import 'package:e_clot_shop/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/models/background_model.dart';
 import '../../../../core/models/category_item_model.dart';
 import '../../../../core/widgets/custom_background_container.dart';
+import '../../../theme/presentation/manager/change_theme/change_theme_cubit.dart';
 
 class CategoriesViewListTile extends StatelessWidget {
   const CategoriesViewListTile({super.key, required this.categoryItem});
@@ -15,10 +18,14 @@ class CategoriesViewListTile extends StatelessWidget {
       backgroundModel: BackgroundModel(
         height: 64,
         padding: 0.0,
+        color: context.read<ChangeThemeCubit>().isDarkMode
+            ? AppColors.darkModeSecondryColor
+            : null,
         child: Center(
           child: ListTile(
             leading: Image.asset(categoryItem.image, height: 40),
-            title: Text(categoryItem.title, style: Styles.styleMedium16(context)),
+            title:
+                Text(categoryItem.title, style: Styles.styleMedium16(context)),
           ),
         ),
       ),
