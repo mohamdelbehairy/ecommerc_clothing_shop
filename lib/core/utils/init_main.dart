b91fcd61e09ed1b61f233a8f6c5a600fde3a134f
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-06 00:20:23 +0200

    done

--- lib/core/utils/init_main.dart
+++ lib/core/utils/init_main.dart
@@ -2,6 +2,7 @@ import 'package:e_clot_shop/core/utils/bloc_observer.dart';
 import 'package:e_clot_shop/core/utils/secret_key.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 
@@ -10,6 +11,9 @@ import 'setup_service_locator.dart';
 
 Future<void> initMain() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   Stripe.publishableKey = SecretKey.stripPublishableKey;
   Bloc.observer = SimpleBlocObserver();
   setupServiceLocator();
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'core/utils/init_main.dart';
 import 'features/app/presentation/widgets/e_clot_shop.dart';
 
 void main() async {
   await initMain();
-  runApp(DevicePreview(
-    enabled: false,
-    builder: (context) => const EClotShop(),
-  ));
+  runApp(const EClotShop());
 }
